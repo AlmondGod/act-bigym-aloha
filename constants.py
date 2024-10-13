08ea2f4908a3481fc2683fc1f75650eefabc8b40
AlmondGod <anandmajmudar1@gmail.com>
2024-10-12 21:58:06 -0400

    colab data location adjust

--- constants.py
+++ constants.py
@@ -1,7 +1,9 @@
 import pathlib
 
 ### Task parameters
-DATA_DIR = '/Users/almondgod/Repositories/aloha-bigym/control/data'
+ROOT = '/Users/almondgod/Repositories'
+COLAB_ROOT = '/content'
+DATA_DIR = COLAB_ROOT + '/aloha-bigym/control/data'
 SIM_TASK_CONFIGS = {
     'sim_transfer_cube_scripted':{
         'dataset_dir': DATA_DIR + '/sim_transfer_cube_scripted',
